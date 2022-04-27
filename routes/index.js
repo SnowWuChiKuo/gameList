e6450c61d8c83035555f11612d4792a2c6d6e02b
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-04-27 22:22:12 +0800

    add CRUD grandChase

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ const express = require('express')
 const router = express.Router()
 // 引入 maples 模組程式碼
 const maples = require('./modules/maples')
+// 引入 grands 模組程式碼
+const grands = require('./modules/grand')
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
-// 將網址結構符合 /maples 字串開頭的 request 導向 maples 模組
+// 將網址結構符合 /mapleStorys 字串開頭的 request 導向 maples 模組
 router.use('/mapleStorys', maples)
+// 將網址結構符合 /grandChases 字串開頭的 request 導向 grands 模組
+router.use('/grandChases', grands)
 
 
 // 匯出路由器
---
+++ routes/modules/grand.js
@@ -0,0 +1,69 @@
+// 引用 express 與 Express 路由器
+const express = require('express')
+const grand = require('../../models/grand')
+const router = express.Router()
+// 引用 Maple model
+const Grand = require('../../models/grand')
+
+// 瀏覽 GrandChase 新增頁面
+router.get('/new', (req, res) => {
+  return res.render('newG')
+})
+// 將新增內容放入 瀏覽 GrandChase 頁面
+router.post('/', (req, res) => {
+  const { name, url, info } = req.body
+  return Grand.create({ name, url, info })
+    .then(() => res.redirect('/GrandChase'))
+    .catch(error => console.log(error))
+})
+// 進入瀏覽頁觀看細部內容
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Grand.findById(id)
+    .lean()
+    .then((grand) => res.render('detailG', {grand}))
+    .catch(error => console.log(error))
+})
+// 進入編輯頁
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Grand.findById(id)
+    .lean()
+    .then((grand) => res.render('editG', { grand }))
+    .catch(error => console.log(error))
+})
+// 修改編輯內容
+router.put('/:id', (req, res) => {
+  const _id = req.params.id
+  const body = req.body
+
+  return Grand.findById(_id)
+    .then(grand => {
+      grand.name = body.name
+      grand.url = body.url
+      grand.info = body.info
+      return grand.save()
+    })
+    .then(() => res.redirect(`/grandChases/${_id}`))
+    .catch(error => console.log(error))
+})
+// 刪除此內容
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Grand.findById(id)
+    .then(grand => grand.remove())
+    .then(() => res.redirect('/grandChase'))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
+
+
+
+
+
+
+
+
+
+module.exports = router
